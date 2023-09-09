8193a50689c6ef42af76f84cdd3b2f17115a42ba
claytony <silvaclaytony73@gmail.com>
2023-09-09 12:06:09 -0300

    feat: inserindo Entrada e saida de dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in, "UTF-8")) {
+            
+        	System.out.print("Por favor, digite o numero da conta: ");
+        	int conta = sc.nextInt();
+        	sc.nextLine();
+        	
+            System.out.print("Por favor, digite o numero da agencia: ");
+            int numero = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Por favor, digite o nome do cliente: ");
+            String nomeCliente = sc.nextLine();
+
+            System.out.print("Por favor, digite o saldo da conta: ");
+            double saldo = sc.nextDouble();
+            sc.nextLine();
+            
+           
+
+            JOptionPane.showMessageDialog(null, " Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! ");
+            JOptionPane.showMessageDialog(null, "Sua agencia é " + numero + ", conta " + conta + " e seu saldo é de R$" + saldo + ".");
+            
+            //System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
+            //System.out.println("Sua agência é " + numero + ", conta " + conta + " e seu saldo é de R$" + saldo + ".");
+            sc.close();
+        }
     }
 }
+
+
+
+       
+    
+
+
